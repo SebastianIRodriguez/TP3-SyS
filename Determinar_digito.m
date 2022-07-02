fa6b204777e663684f43f037483808c323d0d88e
raffaluciano <lraffagnini21@gmail.com>
2022-07-02 19:49:03 -0300

    Mi primer commit desde Visual Studio

---
+++ Determinar_digito.m
@@ -0,0 +1,50 @@
+function [digito] = Determinar_digito(f1, f2)
+%{
+Realice una función que tome como argumentos de entrada dos frecuencias, F1 y F2 (F1 < F2), y como
+argumento de salida la variable digito, correspondiente al dígito identificado. Para ello compare F1
+con cada una de las frecuencias FL y F2 con cada una de las frecuencias FH. En caso de que alguna de las
+frecuencias estuviera fuera del rango (FL ± 1.8% y FH ± 1.8%), la variable de salida debe ser
+digito=[].
+%}
+
+    Fl = [697, 770, 852, 941];
+    Fh = [1209, 1336, 1477, 1633];
+
+    keyboard = [
+        '1', '2', '3', 'A';
+        '4', '5', '6', 'B';
+        '7', '8', '9', 'C';
+        '*', '0', '#', 'D';
+    ];
+
+    p_low = 0;
+    p_high = 0;
+
+    %Identificar fila en el teclado
+    for i = 1:4
+
+        if (f1 > Fl(1, i) * 0.982 && f1 < Fl(1, i) * 1.018)
+            p_low = i;
+        end
+
+    end
+
+    %Identificar columna en el teclado
+    for i = 1:4
+
+        if (f2 > Fh(1, i) * 0.982 && f2 < Fh(1, i) * 1.018)
+            p_high = i;
+        end
+
+    end
+
+    if(p_low == 0 || p_high == 0) 
+        digito = ' ';
+    else
+        digito = keyboard(p_low, p_high);
+    
+    end
+
+    
+
+end
--- Ej1.m
+++ Ej1.m
@@ -1,38 +1,56 @@
-%load('datosADSL.mat');
-
 clear, clc;
 
-load gong.mat;
+%********* CARGAMOS LOS DATOS 
+load('datosADSL.mat');
+y = datosADSL;
 
-Fs = 2208001;
+% La frecuencia m�xima de la se�al es 1.104e6 Hz
+Fs = 2 * 1.104e6;
 Ts = 1/Fs;
-n = 2097152;
 
-y=datosADSL;
 x=[0:1:length(y)-1];
-
 x=x*Ts;
 
-%plot(x,y);
+%{
+%********* GRAFICAMOS datosADSL 
+plot(x,y);
+xlabel('Tiempo [s]');
+ylabel('Magnitud');
+%}
+
 
 %********* SACAMOS LA TRANSFORMADA
+n = 2^21;
 Y=fft(y,n);
+% 2^21 es la potencia de 2 inmediatamente superior a la longitud de la
+% muestra
 
-YY=abs(y);
-frec=[0:2*pi/length(YY):2*pi];
-frec=frec(:,1:length(YY));
+frec=[-Fs/2 : Fs/n : Fs/2 - Fs/n];
 
-%********* GRAFICAMOS LA TRANSFORMADA
-plot(frec,YY);xlabel('frecuencia w [rad/seg]');
+%********* GRAFICAMOS LA TRANSFORMADA EN FUNCI�N DE FREQ. CONTINUAS
+plot(frec,fftshift(abs(Y)));
+xlabel('Frecuencia [Hz]');
 ylabel('Magnitud');
 
-k_voz_i = floor((300/Fs) *length(YY));
-k_voz_s = ceil((3400/Fs) *length(YY));
+k_voz_i = floor((300/Fs) * n);
+k_voz_s = ceil((3400/Fs) * n);
 
-filtervoz = [zeros(1,k_voz_i),ones(1,k_voz_s-k_voz_i),zeros(1,length(YY)-k_voz_s)];
+filtervoz = zeros(n,1);
+filtervoz(k_voz_i:k_voz_s) = 1;
+filtervoz(n - k_voz_s:n - k_voz_i) = 1;
 
-VOZ = filtervoz .* YY';
+VOZ = filtervoz .* Y;
 
-plot(VOZ(:,k_voz_i:k_voz_s));
+%plot(frec,abs(fftshift(VOZ)));
 
-voz = ifft(VOZ);
+%plot(VOZ(:,k_voz_i:k_voz_s));
+
+voz = real(ifft(VOZ,n));
+
+Fs_audio = 2^13;
+
+%soundsc(voz);
+y_resample = resample(voz, Fs_audio, Fs);
+soundsc(y_resample, Fs_audio);
+soundsc(y_resample, Fs_audio*2);
+soundsc(y_resample, Fs_audio/2);
